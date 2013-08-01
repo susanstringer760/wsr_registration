10edac5b4b03f83f2b33cc0c2975403bfaedf4fd
Susan Stringer <snorman@ucar.edu>
2013-08-01 16:44:08 -0600

    app/controllers/application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   before_filter :set_constants
 
   protected
-  def set_constants
 
+  def set_constants
 
     # room rates
     @prices = Hash.new
@@ -21,8 +21,8 @@ class ApplicationController < ActionController::Base
     @occupancy_by_value = {'Single'=>'1','Double'=>'2', 'Triple'=>'3' }
 
     # report recipients
-    #@email_list = "sanityhasreturned@gmail.com,sensabama@aol.com,susanstringer760@comcast.net"
-    @email_list = "susanstringer760@comcast.net, snorman@ucar.edu"
+    @email_list = "sanityhasreturned@gmail.com,sensabama@aol.com,susanstringer760@comcast.net"
+    #@email_list = "susanstringer760@comcast.net, snorman@ucar.edu"
 
     # base scholarship amount
     @initial_scholarship = 300.00
@@ -34,21 +34,21 @@ class ApplicationController < ActionController::Base
 
   end
 
-  def set_prices() 
-    @prices = Hash.new
-    @prices[1] = 364 
-    @prices[2] = 235 
-    @prices[3] = 195 
-  end 
-
-  def set_occupancy
-    @occupancy_by_id = {'1'=>'Single','2'=>'Double', '3'=>'Triple' }
-    @occupancy_by_value = {'Single'=>'1','Double'=>'2', 'Triple'=>'3' }
-  end
-
-  def report_email
-    @email_list = "sanityhasreturned@gmail.com,sensabama@aol.com,susanstringer760@comcast.net"
-    #@email_list = "susanstringer760@comcast.net, snorman@ucar.edu"
-  end
+#  def set_prices() 
+#    @prices = Hash.new
+#    @prices[1] = 364 
+#    @prices[2] = 235 
+#    @prices[3] = 195 
+#  end 
+#
+#  def set_occupancy
+#    @occupancy_by_id = {'1'=>'Single','2'=>'Double', '3'=>'Triple' }
+#    @occupancy_by_value = {'Single'=>'1','Double'=>'2', 'Triple'=>'3' }
+#  end
+#
+#  def report_email
+#    @email_list = "sanityhasreturned@gmail.com,sensabama@aol.com,susanstringer760@comcast.net"
+#    #@email_list = "susanstringer760@comcast.net, snorman@ucar.edu"
+#  end
 
 end
